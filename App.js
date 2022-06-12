a588577ce535c0118cc567bee46f7f027a81d023
Kax675 <kaxozae@gmail.com>
2022-06-12 13:57:36 +0300

    V1.0

--- App.js
+++ App.js
@@ -5,15 +5,10 @@ function createWindow () {
   const win = new BrowserWindow({
      fullscreen: true,
      frame: false,
-     webPreferences: {
-        webviewTag: true,
-        nodeIntegration: true,
-        contextIsolation: false,
-
-     }
   })
 
-  win.loadFile('index.html')
+  win.loadURL('https://youtube.com/tv')
+  win.webContents.userAgent = 'Mozilla/5.0 (compatible; U; NETFLIX) AppleWebKit/533.3 (KHTML, like Gecko) Qt/4.7.0 Safari/533.3 Netflix/3.2 (DEVTYPE=RKU-42XXX-; CERTVER=0) QtWebKit/2.2, Roku 3/7.0 (Roku, 4200X, Wireless)'
 }
 
 app.whenReady().then(() => {
--- Root.js
+++
@@ -1,9 +0,0 @@
-const webview = document.querySelector('webview')
-webview.addEventListener("dom-ready", function () {
-webview.setUserAgent("Mozilla/5.0 (compatible; U; NETFLIX) AppleWebKit/533.3 (KHTML, like Gecko) Qt/4.7.0 Safari/533.3 Netflix/3.2 (DEVTYPE=RKU-42XXX-; CERTVER=0) QtWebKit/2.2, Roku 3/7.0 (Roku, 4200X, Wireless)");
-webview.insertCSS(`
-* {
-    transition: all 100ms !important;
-}
-`);
-});
